654facc5a39ca0018e8c1f91a098b758155dab5a
nashiraoksani <nashiraaoksani@mail.ugm.ac.id>
2023-02-27 19:56:53 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -2,17 +2,25 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 
+// albums
 const albums = require('./api/albums');
 const AlbumsService = require('./services/postgres/AlbumsService');
 const AlbumsValidator = require('./validator/albums');
 
+// songs
 const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
 const SongsValidator = require('./validator/songs');
 
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -24,21 +32,45 @@ const init = async () => {
     }
   });
 
-  await server.register({
-    plugin: albums,
-    options: {
-      service: albumsService,
-      validator: AlbumsValidator
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumsValidator
+      }
+    },
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator
+      }
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator
+      }
     }
-  });
+  ]);
 
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator
-    }
-  });
+  // await server.register({
+  //   plugin: albums,
+  //   options: {
+  //     service: albumsService,
+  //     validator: AlbumsValidator
+  //   }
+  // });
+
+  // await server.register({
+  //   plugin: songs,
+  //   options: {
+  //     service: songsService,
+  //     validator: SongsValidator
+  //   }
+  // });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
